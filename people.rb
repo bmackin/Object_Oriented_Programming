d88a485b1ae08d73fc8b4c3f681265ad953ff32f
bmackin <mackin.beth@gmail.com>
2014-01-16 13:24:10 -0500

    commit 1-8

--- people.rb
+++ people.rb
@@ -1,15 +1,39 @@
-# commit 1-2
+# commit 4
 
-class Student
+class Person
+	def initialize(name)
+	@name = name
+	end
+
+	def greeting
+		puts "Hi, my name is #{@name}."
+	end
+end
+
+class Student < Person
 
 	def learn
 		puts "i get it!"
 	end
 end
 
-class Instructor
+class Instructor < Person
 
 	def teach
 		puts "Everything in Ruby is an Object."
 	end
 end
+
+
+chris = Instructor.new("Chris")
+chris.greeting
+christina = Student.new("Christina")
+christina.greeting
+chris.teach
+christina.learn
+christina.teach #does not work because the teach method is in the instructor class.
+
+
+
+
+
